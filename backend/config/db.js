92a442079305276b6281bd03927093f14526a190
Muhammed Anzaf <anzafmuhammedpc@gmail.com>
2026-07-13 21:10:54 +0530

    Connect backend to MongoDB Atlas

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,15 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try{
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("DATABSE CONNECTED SUCCESSFULLY");
-    } catch(error){
-        console.log("DATABASE CONNECTION FAILED");
-        console.log("ERROR");
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("DATABASE CONNECTED");
+  } catch (error) {
+    console.log("DATABASE CONNECTION FAILED");
+    console.error(error);  
+    process.exit(1);
+  }
 };
 
 export default connectDB;
